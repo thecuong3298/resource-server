a6f98b3408adc4052e81c5506423491690f99c5b
cuongdt <cuongdt@ngscds.vn>
2022-07-18 15:13:06 +0700

    up ver

--- src/main/java/com/amilo/userservice/controller/PublicController.java
+++ src/main/java/com/amilo/userservice/controller/PublicController.java
@@ -1,16 +1,17 @@
 package com.amilo.userservice.controller;
 
-import com.common.dto.ResponseWrapper;
+import com.common.resourceserver.dto.Token;
 import com.common.rest.CustomRestTemplate;
-import com.common.rest.error.CommonException;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames.ACCESS_TOKEN;
 
 @RestController
 @RequestMapping("public")
@@ -24,7 +25,21 @@ public class PublicController {
 
 
     @GetMapping("test")
-    public String test(Authentication authentication) {
-        return "restResponse";
+    public Token getToken(HttpServletResponse response) {
+        String cookie = "%s=%s; Path=/; HttpOnly; SameSite=None; Secure=false";
+        response.addHeader(HttpHeaders.SET_COOKIE,
+                String.format(cookie, ACCESS_TOKEN, "BBXWsMgFDOGWOJaIX7wFdbN-jOU"));
+        Cookie cookie1 = new Cookie("refresh", "1234");
+        cookie1.setMaxAge(60);
+        cookie1.setHttpOnly(true);
+        cookie1.setSecure(false);
+        cookie1.setPath("/");
+        response.addCookie(cookie1);
+        return new Token();
+    }
+
+    @GetMapping("check")
+    public void check(HttpServletRequest request) {
+        System.out.println(request.getCookies());
     }
 }
